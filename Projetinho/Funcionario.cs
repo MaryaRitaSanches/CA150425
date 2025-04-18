c8d0f8b1793259cbe1440d400c8d9e5bd18b6024
Marya Rita Sanches <162769276+MaryaRitaSanches@users.noreply.github.com>
2025-04-18 13:12:19 -0300

    Atualizar o Funcionario.cs

--- Projetinho/Funcionario.cs
+++ Projetinho/Funcionario.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,27 +14,42 @@ namespace Projetinho
 
         public void definirSalario()
         {
-            
+            Console.WriteLine("Digite o seu registro:");
+            this.registro = int.Parse(Console.ReadLine());
+            if (registro <= 1729)
+            {
+                Console.WriteLine("Digite o seu salario é de R$ 1.500,00", this.salario);
+            }
+            else if (registro <= 2637328)
+            {
+                Console.WriteLine("Digite o seu salario é de R$ 2.500,00", this.salario);
+            }
+            else 
+            {
+                Console.WriteLine("Digite o seu salario é de R$ 4.500,00",this.salario);
+            }
         }
         public void exibirSalario()
         {
-
+            Console.WriteLine("Digite o seu registro:");
+            this.registro = int.Parse(Console.ReadLine());
+            Console.WriteLine("O seu salario é de R$ {0}", this.salario);
         }
-        public void Desconto()
+        public double Descontos(double valorCompra)
         {
-
+            Console.WriteLine("Digite o valor da compra:");
+            valorCompra = double.Parse(Console.ReadLine());
+            return valorCompra * 0.1;
         }
-        public void Bonus()
+        public double Bonus()
         {
-
-        }
-        public void cadastrarRegistro()
-        {
-
+            Console.WriteLine("Digite o seu salario:");
+            this.salario = double.Parse(Console.ReadLine());
+            return salario * 0.5;
         }
         public void consultarRegistro()
         {
-
+             
         }
         public void adicionarDependente()
         {
